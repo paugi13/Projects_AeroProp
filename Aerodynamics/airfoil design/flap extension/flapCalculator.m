c458a444c0c313a9778057fa171381d0ec3b458f
Pau Gil Corretger <68238497+paugi13@users.noreply.github.com>
2022-10-24 19:39:40 +0200

    added gap and overlap for fowler flap

--- Aerodynamics/airfoil design/flap extension/flapCalculator.m
+++ Aerodynamics/airfoil design/flap extension/flapCalculator.m
@@ -1,14 +1,17 @@
 function posFlap = flapCalculator(rotAngle, posFlap, pointFlap, xRel)
 % Function that rotates the flap's camber line. 
 
+overlap = -0.001;
+gap = -0.02041;
+
 coord_rel = zeros(2,1);
 for i=1:pointFlap
     coord_rel(:, 1) = posFlap(i, :);
     rot_mat = [cosd(-rotAngle) -sind(-rotAngle); sind(-rotAngle)...
         cosd(-rotAngle)];
     new_coord_rel = rot_mat*coord_rel;
-    posFlap(i, 1) = new_coord_rel(1) + xRel;
-    posFlap(i, 2) = new_coord_rel(2);
+    posFlap(i, 1) = new_coord_rel(1) + xRel + overlap;
+    posFlap(i, 2) = new_coord_rel(2) + gap;
 end
 
 end
