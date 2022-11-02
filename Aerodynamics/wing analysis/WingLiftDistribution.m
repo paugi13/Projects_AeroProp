adb496cd3423f606856274da52c6f93d31b31b46
Pau Gil Corretger <68238497+paugi13@users.noreply.github.com>
2022-11-02 21:12:41 +0100

    added total lift integration

--- Aerodynamics/wing analysis/WingLiftDistribution.m
+++ Aerodynamics/wing analysis/WingLiftDistribution.m
@@ -1,24 +1,40 @@
 %% Lift computation
 
-%% Load workspaces
+%% Load workspace for desired incidence angle
 % Only one analysis at the time
-load('wing analysis/workspaces/wingLiftdist5');
 
+wantedAoA = input('Wing incidence angle?: ');
+direct = join(['wing analysis/workspaces/wingLiftdist', num2str(wantedAoA)]);
+disp(join(['wing analysis/workspaces/wingLiftdist', num2str(wantedAoA)]));
+load(direct);
 
 %% Input data
 % cruise regime: MTOW?
 MTOW = 39850*9.81;
+cruiseWeight = MTOW * 0.85;
 cruiseSpeed = 857/3.6;
 rho = 0.363918;
 q = 0.5*rho*cruiseSpeed^2;
 
+
+%% Calculations
 liftDist = 2*q*wingChord*cl_local(N/2+1:end, 12).*wingChord(N/2+1:end);
+
+d = 5;
+dist = @(x) 0;
+for i = 1:length(polinomialFit)
+    dist = dist + polinomialFit(i)*x^d;
+    d = d - 1;
+end
+
+TotalLift = 2*q*integral(dist, spanCoords(1), spanCoords(end));
+
 
 %% Plot lift distribution
 fig1 = figure(1);
 hold on
 title("\textbf{Local $C_l$ vs. Spanwise station }");
-plot(spanCoords, cl_local(N/2+1:end, 12).*wingChord(N/2+1:end), 'b', 'LineWidth', 1);
+plot(spanCoords, Cl_Values, 'b', 'LineWidth', 1);
 xlabel("$x/b$ $\left[\mathrm{-}\right]$");
 ylabel("$C_l$ $\left[\mathrm{-}\right]$");
 grid on;
