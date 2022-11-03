d298b61dde65a8f349b85ec17924e37cbfc08702
Pau Gil Corretger <68238497+paugi13@users.noreply.github.com>
2022-11-03 11:59:58 +0100

    cruise and TO regimes

--- Aerodynamics/wing analysis/WingLiftDistribution.m
+++ Aerodynamics/wing analysis/WingLiftDistribution.m
@@ -6,23 +6,35 @@ addpath(genpath(fileparts(mfilename('fullpath'))));
 %% Load workspace for desired incidence angle
 % Only one analysis at the time
 
+flightReg = input('Takeoff or cruise regime? (1/0): ' );
 wantedAoA = input('Wing incidence angle?: ');
-direct = join(['wing analysis/workspaces/wingLiftdist', num2str(wantedAoA)]);
-disp(join(['wing analysis/workspaces/wingLiftdist', num2str(wantedAoA)]));
-load(direct);
+
 
 %% Input data
 % cruise regime: MTOW?
 MTOW = 39850*9.81;
-cruiseWeight = MTOW * 0.85;
-cruiseSpeed = 857/3.6;
-rho = 0.363918;
-q = 0.5*rho*cruiseSpeed^2;
 
 %% Calculations
-liftDist = 2*q*Cl_Values;
+if flightReg == 1   %takeoff
+    TOW = MTOW;
+    TOSpeed = 270.392;
+    rho = 0.974;
+    q = 0.5*rho*TOSpeed^2;
+    direct = join(['wing analysis/workspaces/wingLiftdistFlap', ...
+        num2str(wantedAoA)]);
+    load(direct);
+else    %cruise
+    cruiseWeight = MTOW * 0.85;
+    cruiseSpeed = 857/3.6;
+    rho = 0.363918;
+    q = 0.5*rho*cruiseSpeed^2;
+    direct = join(['wing analysis/workspaces/wingLiftdist', ...
+        num2str(wantedAoA)]);
+    load(direct);
+end
 
-d = 6;
+liftDist = 2*q*Cl_Values;
+d = 6;      % polynomial degree = d - 1.
 dist = @(x) 0;
 for i = 1:length(polinomialFit)
     dist = @(x) dist(x) + polinomialFit(i)*x.^d;
